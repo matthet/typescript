e1fe4c622276dae5f19037070e60c1726b3e3fdc
Tara Matthews <tara.matthews@mastercard.com>
2018-02-19 19:35:41 +0100

    initial commit

---
+++ helloWorld/helloWorld.js
@@ -0,0 +1,5 @@
+//console.log('hello world');
+function hello(string) {
+    console.log('Hello ' + string);
+}
+hello('John');
---
+++ helloWorld/helloWorld.ts
@@ -0,0 +1,7 @@
+//console.log('hello world');
+
+function hello(string : String) {
+	console.log('Hello ' + string);
+}
+
+hello('John');
